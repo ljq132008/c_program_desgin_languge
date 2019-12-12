76bd5847507634b1a2dade3e199c65a9b83e96aa
ljq132008 <124266585@qq.com>
2019-12-13 00:28:27 +0800

    华氏转摄氏

--- 0.helloword/helloworld.c
+++ 0.helloword/helloworld.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+
+void fahr_map_celsius(){
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0; /* 温度表的下限 */
+    upper = 300; /* 温度表的上限 */
+    step = 20; /* 步长 */
+
+    fahr = lower;
+    while(fahr <= upper){
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
 int main(){
     int a = 10;
     int * pa = &a;
     printf("%d\n", *pa);
-    printf("helloworld!");
+    printf("helloworld!\n");
+    fahr_map_celsius();
 }
